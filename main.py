8a81749915b64b665b340896786427edadc4612d
ofir ozeri <ofiroz91@gmail.com>
2021-11-15 18:06:05 +0200

    dean's push

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
+from lxml import etree
 
 app = Flask(__name__)
 
@@ -11,16 +12,23 @@ def hello_world():
     return render_template('index.html', name="dean soffer")
 
 
-# perform extraction of the data
-def extractData(soup):
+# perform extraction of the data by user choice of type
+def extractData(soup, r = ''):
     data = []
     symbols = request.form['info']
     dataType = request.form['dataType']
     if(dataType == 'Tags'):
-        for dat in symbols.splitlines(): data.append(soup.find_all(dat))
+        for dat in symbols.splitlines():
+            data.append(soup.find_all(dat))
     elif(dataType == 'CSS'):
-        for dat in symbols.splitlines(): data.append(soup.select(dat))
-    #elif(dataType=='xPath'): for dat in symbols.splitlines():
+        for dat in symbols.splitlines():
+            data.append(soup.select(dat))
+    elif(dataType=='xPath'):
+        dom = etree.HTML(r)
+        #print(dom.xpath(symbols))
+        for dat in symbols.splitlines():
+            print(dom.xpath(dat))
+            data.append(dom.xpath(dat).text)
     elif(dataType=='Tag id'):
         for dat in symbols.splitlines(): data.append(soup.find(id=dat))
 
@@ -32,9 +40,8 @@ def start_crawl():
     url = request.form['url']
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    # soup = defineSoup(response)
     symbols = request.form['info']
-    resultsToSend = extractData(soup)
+    resultsToSend = extractData(soup,response.content)
 
     return render_template('results.html', name="crawling!!", url=url, results=resultsToSend)
 
