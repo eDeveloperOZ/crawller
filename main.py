bdca272746d9abed28b0fb0993283c24cd41fc6e
ofir ozeri <ofiroz91@gmail.com>
2021-11-15 15:52:23 +0200

    This version can differentiate between tags/ID's/CSS and scrape the specified data requested

--- main.py
+++ main.py
@@ -11,16 +11,30 @@ def hello_world():
     return render_template('index.html', name="dean soffer")
 
 
+# perform extraction of the data
+def extractData(soup):
+    data = []
+    symbols = request.form['info']
+    dataType = request.form['dataType']
+    if(dataType == 'Tags'):
+        for dat in symbols.splitlines(): data.append(soup.find_all(dat))
+    elif(dataType == 'CSS'):
+        for dat in symbols.splitlines(): data.append(soup.select(dat))
+    #elif(dataType=='xPath'): for dat in symbols.splitlines():
+    elif(dataType=='Tag id'):
+        for dat in symbols.splitlines(): data.append(soup.find(id=dat))
+
+    return data
+
 @app.route("/start-crawl", methods=['POST'])
 def start_crawl():
 
     url = request.form['url']
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    symbols = request.form['tags']
-    resultsToSend = []
-    for tag in symbols.splitlines():
-        resultsToSend.append(soup.find_all(tag))
+    # soup = defineSoup(response)
+    symbols = request.form['info']
+    resultsToSend = extractData(soup)
 
     return render_template('results.html', name="crawling!!", url=url, results=resultsToSend)
 
